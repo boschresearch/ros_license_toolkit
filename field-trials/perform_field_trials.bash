aaa27308a30416dd33e2ba01ef1371155aa66d97
Christian Henkel <christian.henkel2@de.bosch.com>
2022-12-08 15:36:29 +0100

    Adding some field trials
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

---
+++ field-trials/perform_field_trials.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+i="ros_license_linter"
+type -P $i &>/dev/null  && echo "$i installed"  || { echo "$i command not found."; exit 1; }
+
+workdir="$(pwd)"
+echo "Working directory is $workdir"
+
+if [[ "$workdir" != *field-trials ]]; then
+    echo "Please run this script from the field-trials directory"
+    exit 1
+fi
+
+tmpdir=$(mktemp -d)
+echo "Temporary directory is $tmpdir"
+
+for n in $(cat _repos.list); do
+    echo "Working on $n repository now"
+    cd $tmpdir
+    git clone $n
+    name_w_ext=$(basename $n)
+    name="${name_w_ext%.*}"
+    cd $name
+    echo "Running license linter on $name"
+    ros_license_linter . > $workdir/$name.log
+done
+
+rm -rf $tmpdir
