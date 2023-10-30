8881a9811ac8e34f293e071487cadf17105b1974
Christian Henkel <christian.henkel2@de.bosch.com>
2023-10-30 12:58:57 +0100

    isort
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- src/ros_license_toolkit/main.py
+++ src/ros_license_toolkit/main.py
@@ -30,8 +30,8 @@ from ros_license_toolkit.checks import (LicensesInCodeCheck,
                                         LicenseTextExistsCheck)
 from ros_license_toolkit.package import get_packages_in_path
 from ros_license_toolkit.ui_elements import (FAILURE_STR, SUCCESS_STR,
-                                             Verbosity, major_sep,
-                                             minor_sep, red, rll_print_factory)
+                                             Verbosity, major_sep, minor_sep,
+                                             red, rll_print_factory)
 
 
 def main(args: Optional[Sequence[str]] = None) -> int:
