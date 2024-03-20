15a8812d8785de1aa5ce2710fbab9fea47e48960
Anton Utz <uta5fe@bosch.com>
2024-03-20 12:11:07 +0100

    moved check license_text_exists to own file
    Signed-off-by: Anton Utz <uta5fe@bosch.com>

--- src/ros_license_toolkit/main.py
+++ src/ros_license_toolkit/main.py
@@ -25,11 +25,12 @@ import timeit
 from typing import Optional, Sequence
 
 from ros_license_toolkit.checks import (LicenseTagExistsCheck,
-                                        LicenseTagIsInSpdxListCheck,
-                                        LicenseTextExistsCheck, Status)
+                                        LicenseTagIsInSpdxListCheck, Status)
 from ros_license_toolkit.license_file_referenced_check import \
     LicenseFilesReferencedCheck
 from ros_license_toolkit.license_in_code_check import LicensesInCodeCheck
+from ros_license_toolkit.license_text_exists_check import \
+    LicenseTextExistsCheck
 from ros_license_toolkit.package import get_packages_in_path
 from ros_license_toolkit.ui_elements import (FAILURE_STR, SUCCESS_STR,
                                              WARNING_STR, Verbosity, major_sep,
